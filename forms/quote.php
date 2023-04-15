6d9a19e48e371dcfbf6ae864e3a5647227ab6a6a
mattti <119162325+matti377@users.noreply.github.com>
2023-04-15 13:15:21 +0200

    update

--- forms/quote.php
+++ forms/quote.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'info@elchiito.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = 'Request for a quote';
+  $contact->subject = 'Request for informations';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*

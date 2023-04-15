d14ab84e6123baca65a0d90022e5b1ce9025b63a
mattti <119162325+matti377@users.noreply.github.com>
2023-04-15 20:50:07 +0200

    e

--- login.php
+++ login.php
@@ -1,40 +1,18 @@
 <?php
-//include "db.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
+$sname= "localhost";
 
-$mysqli = new mysqli("localhost", "logger", "iaMAevpi0204E.", "elchiito");
+$unmae= "logger";
 
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+$password = "iaMAevpi0204E.";
+
+$db_name = "elchiito";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+
+    echo "Connection failed!";
+
 }
 
-// Get email and password from POST data
-$email = $_POST["uname"];
-$password = $_POST["psw"];
-
-// Prepare a SELECT statement to retrieve the user's password for the entered email
-$stmt = $mysqli->prepare("SELECT password FROM users WHERE email = ?");
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$stmt->bind_result($hashed_password);
-$stmt->fetch();
-$stmt->close();
-
-// Verify the entered password against the hashed password from the database
-if (password_verify($password, $hashed_password)) {
-    // Passwords match, so redirect to allowed.html
-    header("Location: allowed.html");
-    echo "Redirecting...";
-} else {
-    // Passwords don't match, so redirect to denied.html
-    header("Location: denied.html");
-    echo "Access denied.";
-}
-
-// Close the database connection
-$mysqli->close();
-?>

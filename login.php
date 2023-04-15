b5623467f38d6440628a05001837f396a4c3b74f
mattti <119162325+matti377@users.noreply.github.com>
2023-04-15 20:54:20 +0200

    fe

--- login.php
+++ login.php
@@ -1,18 +1,85 @@
-<?php
+<?php 
 
-$sname= "localhost";
+session_start(); 
 
-$unmae= "logger";
+include "db_conn.php";
 
-$password = "iaMAevpi0204E.";
+if (isset($_POST['email_adress']) && isset($_POST['password'])) {
 
-$db_name = "elchiito";
+    function validate($data){
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+       $data = trim($data);
 
-if (!$conn) {
+       $data = stripslashes($data);
 
-    echo "Connection failed!";
+       $data = htmlspecialchars($data);
+
+       return $data;
+
+    }
+
+    $email_adress = validate($_POST['email_adress']);
+
+    $pass = validate($_POST['password']);
+
+    if (empty($email_adress)) {
+
+        header("Location: index.php?error=User Name is required");
+
+        exit();
+
+    }else if(empty($pass)){
+
+        header("Location: index.php?error=Password is required");
+
+        exit();
+
+    }else{
+
+        $sql = "SELECT * FROM users WHERE email='$email_adress' AND password='$pass'";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) === 1) {
+
+            $row = mysqli_fetch_assoc($result);
+
+            if ($row['email'] === $email_adress && $row['password'] === $pass) {
+
+                echo "Logged in!";
+
+                $_SESSION['email'] = $row['email'];
+
+                $_SESSION['name'] = $row['name'];
+
+                $_SESSION['id'] = $row['id'];
+
+                header("Location: home.php");
+
+                exit();
+
+            }else{
+
+                header("Location: index.php?error=Incorect User name or password");
+
+                exit();
+
+            }
+
+        }else{
+
+            header("Location: index.php?error=Incorect User name or password");
+
+            exit();
+
+        }
+
+    }
+
+}else{
+
+    header("Location: index.php");
+
+    exit();
 
 }
-

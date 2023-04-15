765980470a77acbda6a5f5c4f904c80f9e8f1dbc
mattti <119162325+matti377@users.noreply.github.com>
2023-04-15 14:10:12 +0200

    update

--- forms/quote.php
+++ forms/quote.php
@@ -24,14 +24,13 @@
   $contact->subject = 'Request for informations';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+ 
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'elchiitomailforward@gmail.com',
+    'password' => 'iaMAevpi0204M.',
     'port' => '587'
   );
-  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');

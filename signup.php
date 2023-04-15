1b88262248207f811c750b4cef2920c8862b238c
mattti <119162325+matti377@users.noreply.github.com>
2023-04-15 19:15:54 +0200

    added backend support

---
+++ signup.php
@@ -0,0 +1,32 @@
+<?php
+include "db.php";
+// Retrieve the submitted email and password values
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Connect to the database
+$conn = new mysqli($host, $DB_Username, $DB_password, $DB);
+
+// Check for errors in the connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare the SQL statement to insert the new user
+$stmt = $conn->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+$stmt->bind_param("ss", $email, $password);
+
+// Execute the statement and check for errors
+if ($stmt->execute()) {
+  // Redirect to the allowed.html page if the user was added successfully
+  header("Location: signupped.html");
+  exit();
+} else {
+  // Display an error message if the user could not be added
+  echo "Error: " . $stmt->error;
+}
+
+// Close the statement and the database connection
+$stmt->close();
+$conn->close();
+?>

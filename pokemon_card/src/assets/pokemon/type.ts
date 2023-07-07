65e349b897b81f06187543af5d657797fd56a980
mattti <119162325+matti377@users.noreply.github.com>
2023-07-07 18:38:07 +0200

    update

---
+++ pokemon_card/src/assets/pokemon/type.ts
@@ -0,0 +1,25 @@
+export enum Type {
+    Colorless = "Colorless",
+    Fighting = "Fighting",
+    Steel = "Steel",
+    Fire = "Fire",
+    Water = "Water",
+    Grass = "Grass",
+    Lightning = "Lightning",
+    Psychic = "Psychic",
+    Dark = "Dark",
+    Dragon = "Dragon"
+}
+
+export const TypeOrder = [
+    Type.Grass,
+    Type.Fire,
+    Type.Water,
+    Type.Lightning,
+    Type.Fighting,
+    Type.Psychic,
+    Type.Dark,
+    Type.Steel,
+    Type.Dragon,
+    Type.Colorless
+];
